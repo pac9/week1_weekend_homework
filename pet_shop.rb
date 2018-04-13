38878e9585b50e05bc13819b1847e17e46076882
pac9 <pac9@btinternet.com>
2018-04-13 15:04:21 +0100

    up to stock count

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,24 @@
 def pet_shop_name(shop)
   return shop[:name]
 end
+
+def total_cash(cash)
+  return cash[:admin][:total_cash]
+end
+
+# def add_or_remove_cash(pet_shop, cash)
+#   current_cash = pet_shop[:admin][:total_cash]
+#   revised_cash = current_cash + cash
+#   return revised_cash
+# end
+
+
+def pets_sold(sold)
+ return sold[:admin][:pets_sold]
+end
+#
+#
+#
+def stock_count(stock)
+  stock[:pets].length()
+end
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'minitest/rg'
 require_relative '../pet_shop'
 
 class TestPetShop < Minitest::Test
@@ -77,27 +78,27 @@ class TestPetShop < Minitest::Test
     assert_equal("Camelot of Pets", name)
   end
 
-  # def test_total_cash
-  #   sum = total_cash(@pet_shop)
-  #   assert_equal(1000, sum)
-  # end
+  def test_total_cash
+    sum = total_cash(@pet_shop)
+    assert_equal(1000, sum)
+  end
 
   # def test_add_or_remove_cash__add
   #   add_or_remove_cash(@pet_shop,10)
   #   cash = total_cash(@pet_shop)
   #   assert_equal(1010, cash)
   # end
-
+  #
   # def test_add_or_remove_cash__remove
   #   add_or_remove_cash(@pet_shop,-10)
   #   cash = total_cash(@pet_shop)
   #   assert_equal(990, cash)
   # end
 
-  # def test_pets_sold
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(0, sold)
-  # end
+  def test_pets_sold
+    sold = pets_sold(@pet_shop)
+    assert_equal(0, sold)
+  end
 
   # def test_increase_pets_sold
   #   increase_pets_sold(@pet_shop,2)
@@ -105,10 +106,10 @@ class TestPetShop < Minitest::Test
   #   assert_equal(2, sold)
   # end
 
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
+  def test_stock_count
+    count = stock_count(@pet_shop)
+    assert_equal(6, count)
+  end
 
   # def test_all_pets_by_breed__found
   #   pets = pets_by_breed(@pet_shop, "British Shorthair")
