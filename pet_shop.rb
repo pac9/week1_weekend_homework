b48c13d599679125563b6a4cee8946bc174d56d4
pac9 <pac9@btinternet.com>
2018-04-14 15:20:08 +0100

    12 of 17 attempts successful

--- pet_shop.rb
+++ pet_shop.rb
@@ -25,31 +25,36 @@ def pets_sold(sold)
 end
 
 def increase_pets_sold(shop, sold)
- shop[:admin][:pets_sold] += sold
-
+  shop[:admin][:pets_sold] += sold
 end
 
 def stock_count(stock)
   stock[:pets].length()
 end
 
-def pets_by_breed(pet_shop, breed)
-  total = 0
-  for type in pet_shop[:pets]
-      if (breed == type[:breed])
-        total +=1
-      end
-  end
-  return total
-end
+# def pets_by_breed(pet_shop, breed)
+#   # total = 0
+#   # for type in pet_shop[:pets]
+#   #     if breed == type[:breed]
+#   #       total +=1
+#   #     end
+#   # end
+#  return total
+# end
 
-def find_pet_by_name(pet, pet_name)
-  for name in pet[:pets]
-     if (pet_name == name[:name])
-       return name
-     end
-   end
-  return nil
+# def find_pet_by_name(pet, pet_name)
+#   for name in pet[:pets]
+#      if (pet_name == name[:name])
+#        return name
+#      end
+#    end
+#   return nil
+# end
+
+def add_pet_to_stock(pet_shop, pet)
+  pet_shop[:pets].push(pet)
+  p pet_shop[:pets]
+  pet_shop[:pets].length()
 end
 
 def customer_cash(cash)
@@ -58,4 +63,19 @@ end
 
 def customer_pet_count(num_pets)
   num_pets[:pets].length()
+end
+
+# def remove_pet_by_name(pet_shop, name)
+#  pet_shop[:pets].delete(name)
+#
+# end
+
+def remove_customer_cash(customer, amount)
+ customer[:cash] -= amount
+end
+
+def add_pet_to_customer(customer, pet)
+  customer[:pets].push("Donkey")
+  # p customer[:pets]
+  customer[:pets].length()
 end
